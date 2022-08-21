6a1825c6fe2083ab88df9c88f0e9ff69942bdae3
golamrabbani93 <golamrabbani9373@gmail.com>
2022-08-22 02:44:23 +0600

    create two common function

--- js/utilities.js
+++ js/utilities.js
@@ -1,1 +1,18 @@
-console.log("utilities.js");
+// !create common function get input value by id
+
+function getInputValue(id) {
+	const inputValue = document.getElementById(id);
+	const inputValueString = inputValue.value;
+	const input = parseInt(inputValueString);
+	return input;
+}
+
+// !create common function get element text and show inner text by id
+
+function getElementShow(id1, id2) {
+	const getElement = document.getElementById(id1);
+
+	const showElement = id2;
+
+	getElement.innerText = showElement;
+}
